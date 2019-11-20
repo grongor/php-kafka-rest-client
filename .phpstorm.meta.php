1abd37d531a81f52748775f6f0423031a762d55e
Jakub Chábek <jakub.chabek@cdn77.com>
2019-11-20 14:25:20 +0100

    Remove unnecessary var annotations

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -2,8 +2,9 @@
 
 namespace PHPSTORM_META {
 
+    use Grongor\KafkaRest\Api\HttpRestClient;
     use Mockery;
 
     override(Mockery::mock(0), type(0));
+    override(HttpRestClient::execute(1), type(0));
 }
-
